edfccd0cb577e3e3eb11f12609afe88f30ab3f76
MaxieQiu <911619813qq@gmail.com>
2025-11-21 14:22:02 +0100

    1

--- ws/pp/lab_1-V1.0/lab_1/source/import.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-
-#include "DiGraph.h"
-
-DiGraph get_graph_from_file(const std::string path);
--- ws/pp/lab_1-V1.0/lab_1/source/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdint>
-#include <iostream>
-
-int main(int argc, char** argv) {
-	std::cout << "Please feel free to implement your own code in the main.cpp." << std::endl;
-
-	return 0;
-}
